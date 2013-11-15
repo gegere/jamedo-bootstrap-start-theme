324c941a7879ccbda292677c932c45786de54934
Jason Gegere <jason@hgmail.com>
2013-11-14 20:33:11 -0600

    link correction

--- sidebar-portfolio.php
+++ sidebar-portfolio.php
@@ -1,5 +1,5 @@
 <p class="text-center"><a href="/c/quote"><img alt="" src="/wp-content/themes/bs3/assets/img/request_quote@2x.png" width="240" /></a></p>
 
-<p class="text-center"><a href="/products/"><img src="http://placehold.it/250x80&text=TESTIMONIALS"></a></p>
+<p class="text-center"><a href="/portfolio/testimonials/"><img src="http://placehold.it/250x80&text=TESTIMONIALS"></a></p>
 
 <p class="text-center"><a href="/products/"><img src="http://placehold.it/250x80&text=PORTFOLIO%20GALLERY"></a></p>
